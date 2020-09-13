40bc7cd54a7a08a7840477d2c1303d7432350bc9
MADHAVARAO KAKUMANI <madhavaraoms@outlook.com>
2020-09-13 19:37:58 +0530

    Add project files.

---
+++ RnReddyITSchools.Training.Web.Presentation/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RnReddyITSchools.Training.Web.Presentation.MvcApplication" Language="C#" %>
---
+++ RnReddyITSchools.Training.Web.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RnReddyITSchools.Training.Web.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("RnReddyITSchools.Training.Web.UnitTest")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f5ebf7e8-6b43-4896-96e3-601bbfa5f3fa")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
