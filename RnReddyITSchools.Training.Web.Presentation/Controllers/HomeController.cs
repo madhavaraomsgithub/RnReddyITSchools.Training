c5dd0e631b2cdf37c3a166ed63df7de9dc4c2e1a
MADHAVARAO KAKUMANI <madhavaraoms@outlook.com>
2020-09-13 19:45:30 +0530

    Changes

--- RnReddyITSchools.Training.Web.Presentation/Controllers/HomeController.cs
+++ RnReddyITSchools.Training.Web.Presentation/Controllers/HomeController.cs
@@ -11,6 +11,7 @@ namespace RnReddyITSchools.Training.Web.Presentation.Controllers
         public ActionResult Index()
         {
             return View();
+
         }
 
         public ActionResult About()
